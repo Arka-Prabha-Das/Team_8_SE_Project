e32de747479bdbc66b4d3cc4cc8395d56dbc54ef
Arka Prabha Das <57886447+Arka-Prabha-Das@users.noreply.github.com>
2024-04-27 15:15:31 -0400

    Changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getMessaging } from "firebase/messaging";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,5 +25,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const firestore = getFirestore(app);
-export const messaging = getMessaging(app);
+
 export default app;
